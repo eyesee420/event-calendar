ad41a93b4bc9525a30509197ee596b4f51e373ec
eyesee420 <75460564+eyesee420@users.noreply.github.com>
2022-06-09 09:50:32 +0800

    Add files via upload

---
+++ holidayscript.js
@@ -0,0 +1,57 @@
+var newWindow;
+var w, h = 5;
+var width = window.innerWidth;
+var height = window.innerHeight;
+function holidayFIC(){
+ newWindow = open("https://i.ibb.co/fYkNh7g/fic.png", "fic", "width=650, height=450");
+ w = width / 1.5;
+ h = height / 1.5;
+ newWindow.moveBy(w-350, h-250);
+ newWindow.focus();
+
+}
+  function holidaySND(){
+ newWindow = open("https://i.ibb.co/k1gMxWS/snd.png", "snd", "width=650, height=450");
+ w = width / 1.5;
+ h = height / 1.5;
+ newWindow.moveBy(w-350, h-250);
+ newWindow.focus();
+
+}
+function holidayCE(){
+ newWindow = open("https://i.ibb.co/7XnjVgK/ce.png", "ce", "width=650, height=450");
+ w = width / 1.5;
+ h = height / 1.5;
+ newWindow.moveBy(w-350, h-250);
+ newWindow.focus();
+
+}
+function holidayCD(){
+ newWindow = open("https://i.ibb.co/Htbv4PZ/cd.png", "cd", "width=650, height=450");
+ w = width / 1.5;
+ h = height / 1.5;
+ newWindow.moveBy(w-350, h-250);
+ newWindow.focus();
+
+}
+function holidayRD(){
+ newWindow = open("https://i.ibb.co/MG8Xv7h/rd.png", "rd", "width=650, height=450");
+ w = width / 1.5;
+ h = height / 1.5;
+ newWindow.moveBy(w-350, h-250);
+ newWindow.focus();
+
+}
+function holidayNYE(){
+ newWindow = open("https://i.ibb.co/PWPcNZL/nye.png", "nye", "width=650, height=450");
+ w = width / 1.5;
+ h = height / 1.5;
+ newWindow.moveBy(w-350, h-250);
+ newWindow.focus();
+
+}
+
+function closeBrowser(){
+  newWindow.close();
+  window.close();
+}
